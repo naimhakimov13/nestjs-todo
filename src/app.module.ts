d525264d1b2f3bb2d74bc9bcb9b05c6398c114ab
Naim <naimhakimov2001@gmail.com>
2022-07-16 23:02:52 +1000

    delete module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'api'
-      }),
-    TodosModule
   ],
   controllers: [
     AppController
